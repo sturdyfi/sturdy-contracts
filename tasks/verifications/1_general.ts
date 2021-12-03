4f4d8a0e02d11f5c75760eeaecfccbbd08dfa15b
iris112 <nglayi99@gmail.com>
2021-12-03 15:06:08 -0600

    working for testcases for mainnet and goerli

--- tasks/verifications/1_general.ts
+++ tasks/verifications/1_general.ts
@@ -72,130 +72,130 @@ task('verify:general', 'Verify contracts at Etherscan')
       const uiPoolDataProvider = await getUiPoolDataProvider();
       const uiIncentiveDataProvider = await getUiIncentiveDataProvider();
 
-      // const lendingPoolConfiguratorImplAddress = getParamPerNetwork(
-      //   LendingPoolConfigurator,
-      //   network
-      // );
-      // const lendingPoolConfiguratorImpl = notFalsyOrZeroAddress(lendingPoolConfiguratorImplAddress)
-      //   ? await getLendingPoolConfiguratorImpl(lendingPoolConfiguratorImplAddress)
-      //   : await getLendingPoolConfiguratorImpl();
+      const lendingPoolConfiguratorImplAddress = getParamPerNetwork(
+        LendingPoolConfigurator,
+        network
+      );
+      const lendingPoolConfiguratorImpl = notFalsyOrZeroAddress(lendingPoolConfiguratorImplAddress)
+        ? await getLendingPoolConfiguratorImpl(lendingPoolConfiguratorImplAddress)
+        : await getLendingPoolConfiguratorImpl();
 
-      // const lendingPoolCollateralManagerImplAddress = getParamPerNetwork(
-      //   LendingPoolCollateralManager,
-      //   network
-      // );
-      // const lendingPoolCollateralManagerImpl = notFalsyOrZeroAddress(
-      //   lendingPoolCollateralManagerImplAddress
-      // )
-      //   ? await getLendingPoolCollateralManagerImpl(lendingPoolCollateralManagerImplAddress)
-      //   : await getLendingPoolCollateralManagerImpl();
+      const lendingPoolCollateralManagerImplAddress = getParamPerNetwork(
+        LendingPoolCollateralManager,
+        network
+      );
+      const lendingPoolCollateralManagerImpl = notFalsyOrZeroAddress(
+        lendingPoolCollateralManagerImplAddress
+      )
+        ? await getLendingPoolCollateralManagerImpl(lendingPoolCollateralManagerImplAddress)
+        : await getLendingPoolCollateralManagerImpl();
 
-      // const dataProvider = await getSturdyProtocolDataProvider();
+      const dataProvider = await getSturdyProtocolDataProvider();
 
-      // // Address Provider
-      // console.log('\n- Verifying address provider...\n');
-      // await verifyContract(eContractid.LendingPoolAddressesProvider, addressesProvider, [MarketId]);
+      // Address Provider
+      console.log('\n- Verifying address provider...\n');
+      await verifyContract(eContractid.LendingPoolAddressesProvider, addressesProvider, [MarketId]);
 
-      // // Address Provider Registry
-      // console.log('\n- Verifying address provider registry...\n');
-      // await verifyContract(
-      //   eContractid.LendingPoolAddressesProviderRegistry,
-      //   addressesProviderRegistry,
-      //   []
-      // );
+      // Address Provider Registry
+      console.log('\n- Verifying address provider registry...\n');
+      await verifyContract(
+        eContractid.LendingPoolAddressesProviderRegistry,
+        addressesProviderRegistry,
+        []
+      );
 
-      // // Lending Pool implementation
-      // console.log('\n- Verifying LendingPool Implementation...\n');
-      // await verifyContract(eContractid.LendingPool, lendingPoolImpl, []);
+      // Lending Pool implementation
+      console.log('\n- Verifying LendingPool Implementation...\n');
+      await verifyContract(eContractid.LendingPool, lendingPoolImpl, []);
 
-      // // Lending Pool Configurator implementation
-      // console.log('\n- Verifying LendingPool Configurator Implementation...\n');
-      // await verifyContract(eContractid.LendingPoolConfigurator, lendingPoolConfiguratorImpl, []);
+      // Lending Pool Configurator implementation
+      console.log('\n- Verifying LendingPool Configurator Implementation...\n');
+      await verifyContract(eContractid.LendingPoolConfigurator, lendingPoolConfiguratorImpl, []);
 
-      // // Lending Pool Collateral Manager implementation
-      // console.log('\n- Verifying LendingPool Collateral Manager Implementation...\n');
-      // await verifyContract(
-      //   eContractid.LendingPoolCollateralManager,
-      //   lendingPoolCollateralManagerImpl,
-      //   []
-      // );
+      // Lending Pool Collateral Manager implementation
+      console.log('\n- Verifying LendingPool Collateral Manager Implementation...\n');
+      await verifyContract(
+        eContractid.LendingPoolCollateralManager,
+        lendingPoolCollateralManagerImpl,
+        []
+      );
 
-      // // LidoVault implementation
-      // console.log('\n- Verifying LidoVault Implementation...\n');
-      // await verifyContract(eContractid.LidoVault, lidoVaultImpl, []);
+      // LidoVault implementation
+      console.log('\n- Verifying LidoVault Implementation...\n');
+      await verifyContract(eContractid.LidoVault, lidoVaultImpl, []);
 
-      // // IncentiveController implementation
-      // console.log('\n- Verifying IncentiveController Implementation...\n');
-      // await verifyContract(
-      //   eContractid.StakedTokenIncentivesController,
-      //   incentiveControllerImpl,
-      //   [EMISSION_EXECUTOR]
-      // );
+      // IncentiveController implementation
+      console.log('\n- Verifying IncentiveController Implementation...\n');
+      await verifyContract(eContractid.StakedTokenIncentivesController, incentiveControllerImpl, [
+        EMISSION_EXECUTOR,
+      ]);
 
-      // // IncentiveToken implementation
-      // console.log('\n- Verifying IncentiveToken Implementation...\n');
-      // await verifyContract(eContractid.SturdyToken, incentiveTokenImpl, []);
+      // IncentiveToken implementation
+      console.log('\n- Verifying IncentiveToken Implementation...\n');
+      await verifyContract(eContractid.SturdyToken, incentiveTokenImpl, []);
 
-      // // Test helpers
-      // console.log('\n- Verifying  Sturdy  Provider Helpers...\n');
-      // await verifyContract(eContractid.SturdyProtocolDataProvider, dataProvider, [
-      //   addressesProvider.address,
-      // ]);
+      // Test helpers
+      console.log('\n- Verifying  Sturdy  Provider Helpers...\n');
+      await verifyContract(eContractid.SturdyProtocolDataProvider, dataProvider, [
+        addressesProvider.address,
+      ]);
 
-      // // WalletBalanceProvider implementation
-      // console.log('\n- Verifying WalletBalanceProvider Implementation...\n');
-      // await verifyContract(eContractid.WalletBalanceProvider, walletBalanceProvider, []);
+      // WalletBalanceProvider implementation
+      console.log('\n- Verifying WalletBalanceProvider Implementation...\n');
+      await verifyContract(eContractid.WalletBalanceProvider, walletBalanceProvider, []);
 
-      // // UiPoolDataProvider implementation
-      // console.log('\n- Verifying  UiPoolDataProvider Implementation...\n');
-      // await verifyContract(eContractid.UiPoolDataProvider, uiPoolDataProvider, [
-      //   incentiveControllerAddress,
-      //   oracle
-      // ]);
+      // UiPoolDataProvider implementation
+      console.log('\n- Verifying  UiPoolDataProvider Implementation...\n');
+      await verifyContract(eContractid.UiPoolDataProvider, uiPoolDataProvider, [
+        incentiveControllerAddress,
+        oracle,
+      ]);
 
       // UiIncentiveDataProvider implementation
       console.log('\n- Verifying UiIncentiveDataProvider Implementation...\n');
       await verifyContract(eContractid.UiIncentiveDataProvider, uiIncentiveDataProvider, []);
     }
-    // // Lending Pool proxy
-    // console.log('\n- Verifying  Lending Pool Proxy...\n');
-    // await verifyContract(eContractid.InitializableAdminUpgradeabilityProxy, lendingPoolProxy, [
-    //   addressesProvider.address,
-    // ]);
+    // Lending Pool proxy
+    console.log('\n- Verifying  Lending Pool Proxy...\n');
+    await verifyContract(eContractid.InitializableAdminUpgradeabilityProxy, lendingPoolProxy, [
+      addressesProvider.address,
+    ]);
 
-    // // LendingPool Conf proxy
-    // console.log('\n- Verifying  Lending Pool Configurator Proxy...\n');
-    // await verifyContract(
-    //   eContractid.InitializableAdminUpgradeabilityProxy,
-    //   lendingPoolConfiguratorProxy,
-    //   [addressesProvider.address]
-    // );
+    // LendingPool Conf proxy
+    console.log('\n- Verifying  Lending Pool Configurator Proxy...\n');
+    await verifyContract(
+      eContractid.InitializableAdminUpgradeabilityProxy,
+      lendingPoolConfiguratorProxy,
+      [addressesProvider.address]
+    );
 
-    // // Proxy collateral manager
-    // console.log('\n- Verifying  Lending Pool Collateral Manager Proxy...\n');
-    // await verifyContract(
-    //   eContractid.InitializableAdminUpgradeabilityProxy,
-    //   lendingPoolCollateralManagerProxy,
-    //   []
-    // );
+    // Proxy collateral manager
+    console.log('\n- Verifying  Lending Pool Collateral Manager Proxy...\n');
+    await verifyContract(
+      eContractid.InitializableAdminUpgradeabilityProxy,
+      lendingPoolCollateralManagerProxy,
+      []
+    );
 
-    // // LidoVault proxy
-    // console.log('\n- Verifying  LidoVault Proxy...\n');
-    // await verifyContract(eContractid.InitializableAdminUpgradeabilityProxy, lidoVaultProxy, [
-    //   addressesProvider.address,
-    // ]);
+    // LidoVault proxy
+    console.log('\n- Verifying  LidoVault Proxy...\n');
+    await verifyContract(eContractid.InitializableAdminUpgradeabilityProxy, lidoVaultProxy, [
+      addressesProvider.address,
+    ]);
 
-    // // IncentiveController proxy
-    // console.log('\n- Verifying  IncentiveController Proxy...\n');
-    // await verifyContract(eContractid.InitializableAdminUpgradeabilityProxy, incentiveControllerProxy, [
-    //   addressesProvider.address,
-    // ]);
+    // IncentiveController proxy
+    console.log('\n- Verifying  IncentiveController Proxy...\n');
+    await verifyContract(
+      eContractid.InitializableAdminUpgradeabilityProxy,
+      incentiveControllerProxy,
+      [addressesProvider.address]
+    );
 
-    // // IncentiveToken proxy
-    // console.log('\n- Verifying  IncentiveToken Proxy...\n');
-    // await verifyContract(eContractid.InitializableAdminUpgradeabilityProxy, incentiveTokenProxy, [
-    //   addressesProvider.address,
-    // ]);
+    // IncentiveToken proxy
+    console.log('\n- Verifying  IncentiveToken Proxy...\n');
+    await verifyContract(eContractid.InitializableAdminUpgradeabilityProxy, incentiveTokenProxy, [
+      addressesProvider.address,
+    ]);
 
     console.log('Finished verifications.');
   });

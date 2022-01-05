360810c5dc444f1807e69a94f8540e2a43e5a69d
iris112 <nglayi99@gmail.com>
2022-01-05 12:33:30 -0600

    remove unnecessary code

--- contracts/protocol/lendingpool/LidoVault.sol
+++ contracts/protocol/lendingpool/LidoVault.sol
@@ -27,12 +27,6 @@ contract LidoVault is GeneralVault {
   using SafeERC20 for IERC20;
   using PercentageMath for uint256;
 
-  //ToDo: need to think about using registering flow instead of constant value
-  // address constant LIDO = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
-  // address constant CurveswapLidoPool = 0xDC24316b9AE028F1497c275EB9192a3Ea0f67022;
-  // address constant UniswapRouter = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
-  // address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-
   // uniswap pool fee to 0.05%.
   uint24 constant uniswapFee = 500;
 

af5262223e4bc6fbc10163fa36286cd701e6d9d0
iris112 <nglayi99@gmail.com>
2022-01-05 13:40:50 -0600

    working for yearnVault contracts with deposit/withdraw feature on fantom

---
+++ contracts/interfaces/IYearnVault.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.6.12;
+
+interface IYearnVault {
+  function deposit(uint256 _amount, address recipient) external returns (uint256);
+
+  function withdraw(
+    uint256 maxShares,
+    address recipient,
+    uint256 maxLoss
+  ) external returns (uint256);
+}

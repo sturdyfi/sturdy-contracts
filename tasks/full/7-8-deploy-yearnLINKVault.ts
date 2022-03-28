f95e975066fbded26b634549d915efd000582781
iris112 <nglayi99@gmail.com>
2022-03-28 14:59:42 -0500

    [V1-252] deploying yearnLINKVault on fantom mainnet

--- tasks/full/7-8-deploy-yearnLINKVault.ts
+++ tasks/full/7-8-deploy-yearnLINKVault.ts
@@ -1,8 +1,9 @@
 import { task } from 'hardhat/config';
 import { ConfigNames, loadPoolConfig } from '../../helpers/configuration';
 import { deployYearnLINKVault } from '../../helpers/contracts-deployments';
-import { getLendingPoolConfiguratorProxy } from '../../helpers/contracts-getters';
+import { getLendingPoolConfiguratorProxy, getSturdyOracle } from '../../helpers/contracts-getters';
 import { getParamPerNetwork } from '../../helpers/contracts-helpers';
+import { waitForTx } from '../../helpers/misc-utils';
 import { eNetwork, IFantomConfiguration } from '../../helpers/types';
 
 const CONTRACT_NAME = 'YearnLINKVault';
@@ -19,13 +20,28 @@ task(`full:deploy-yearn-link-vault`, `Deploys the ${CONTRACT_NAME} contract`)
 
     const network = process.env.FORK ? <eNetwork>process.env.FORK : <eNetwork>localBRE.network.name;
     const poolConfig = loadPoolConfig(pool);
-    const { ReserveFactorTreasuryAddress } = poolConfig as IFantomConfiguration;
+    const { ReserveFactorTreasuryAddress, ReserveAssets, ChainlinkAggregator } =
+      poolConfig as IFantomConfiguration;
     const treasuryAddress = getParamPerNetwork(ReserveFactorTreasuryAddress, network);
 
     const yearnLINKVault = await deployYearnLINKVault(verify);
     const configurator = await getLendingPoolConfiguratorProxy();
     await configurator.registerVault(yearnLINKVault.address);
     await yearnLINKVault.setTreasuryInfo(treasuryAddress, '1000'); //10% fee
+
+    // Register yvLINK oracle
+    const sturdyOracle = await getSturdyOracle();
+    await waitForTx(
+      await sturdyOracle.setAssetSources(
+        [getParamPerNetwork(ReserveAssets, network).yvLINK],
+        [getParamPerNetwork(ChainlinkAggregator, network).yvLINK]
+      )
+    );
+    console.log(
+      (
+        await sturdyOracle.getAssetPrice(getParamPerNetwork(ReserveAssets, network).yvLINK)
+      ).toString()
+    );
 
     console.log(`${CONTRACT_NAME}.address`, yearnLINKVault.address);
     console.log(`\tFinished ${CONTRACT_NAME} deployment`);

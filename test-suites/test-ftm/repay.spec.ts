fc2dee1d4617a8d1c52495f4e405d6b798d961f5
iris112 <nglayi99@gmail.com>
2022-04-21 11:36:56 -0500

    add variableSlop2Rate working testcases

--- test-suites/test-ftm/repay.spec.ts
+++ test-suites/test-ftm/repay.spec.ts
@@ -332,3 +332,129 @@ makeSuite('Deposit WFTM as collatoral and other as for pool liquidity supplier '
 //     });
 //   });
 // });
+
+// // checked on block number 36543713 with variableRateSlope2
+// makeSuite('Deposit FTM as collatoral and other as for pool liquidity supplier ', (testEnv) => {
+//   it('User1 deposits USDC, User deposits FTM as collatoral and borrows USDC', async () => {
+//     const { usdc, users, pool, yearnVault, oracle } = testEnv;
+//     const ethers = (DRE as any).ethers;
+//     const usdcOwnerAddress = '0xc5ed2333f8a2c351fca35e5ebadb2a82f5d254c3';
+//     const depositor = users[0];
+//     const borrower = users[1];
+//     printDivider();
+
+  
+//     const addressProvider = await getLendingPoolAddressesProvider();
+//     const rates = await deployDefaultReserveInterestRateStrategy(
+//       [
+//         addressProvider.address,
+//         new BigNumber(0.9).multipliedBy(oneRay).toFixed(),
+//         '0',
+//         '0',
+//         new BigNumber(0.1).multipliedBy(oneRay).toFixed(),
+//         '0',
+//         '0',
+//       ],
+//       false
+//     );
+
+//     await impersonateAccountsHardhat(['0x154D73802a6B3324c017481AC818050afE4a0b0A']);
+//     let signer = await ethers.provider.getSigner('0x154D73802a6B3324c017481AC818050afE4a0b0A');
+//     const configurator = await getLendingPoolConfiguratorProxy();
+//     await configurator.connect(signer).setReserveInterestRateStrategyAddress(usdc.address, rates.address);
+
+
+//     const depositUSDC = '7000';
+//     //Make some test USDC for depositor
+//     await impersonateAccountsHardhat([usdcOwnerAddress]);
+//     signer = await ethers.provider.getSigner(usdcOwnerAddress);
+//     const amountUSDCtoDeposit = await convertToCurrencyDecimals(usdc.address, depositUSDC);
+//     await usdc.connect(signer).transfer(depositor.address, amountUSDCtoDeposit);
+
+//     //approve protocol to access depositor wallet
+//     await usdc.connect(depositor.signer).approve(pool.address, APPROVAL_AMOUNT_LENDING_POOL);
+
+//     //Supplier  deposits 7000 USDC
+//     await pool
+//       .connect(depositor.signer)
+//       .deposit(usdc.address, amountUSDCtoDeposit, depositor.address, '0');
+
+//     const supplierGlobalData = await pool.getUserAccountData(depositor.address);
+//     printUserAccountData({
+//       user: `Supplier ${depositor.address}`,
+//       action: 'deposited',
+//       amount: depositUSDC,
+//       coin: 'USDC',
+//       unit: 'USD',
+//       ...supplierGlobalData,
+//     });
+
+//     //user 2 deposits 1000 FTM
+//     const amountFTMtoDeposit = ethers.utils.parseEther('1000');
+//     await yearnVault
+//       .connect(borrower.signer)
+//       .depositCollateral(ZERO_ADDRESS, 0, { value: amountFTMtoDeposit });
+//     {
+//       const supplierGlobalData = await pool.getUserAccountData(borrower.address);
+//       printUserAccountData({
+//         user: `Borrower ${borrower.address}`,
+//         action: 'deposited',
+//         amount: ETHfromWei(amountFTMtoDeposit),
+//         coin: 'FTM',
+//         unit: 'USD',
+//         ...supplierGlobalData,
+//       });
+//     }
+
+//     //user 2 borrows
+//     const userGlobalData = await pool.getUserAccountData(borrower.address);
+//     const usdcPrice = await oracle.getAssetPrice(usdc.address);
+
+//     const amountUSDCToBorrow = await convertToCurrencyDecimals(
+//       usdc.address,
+//       new BigNumber(userGlobalData.availableBorrowsETH.toString())
+//         .div(usdcPrice.toString())
+//         .multipliedBy(0.95)
+//         .toFixed(0)
+//     );
+
+//     await pool
+//       .connect(borrower.signer)
+//       .borrow(usdc.address, amountUSDCToBorrow, RateMode.Variable, '0', borrower.address);
+
+//     const userGlobalDataAfter = await pool.getUserAccountData(borrower.address);
+//     printUserAccountData({
+//       user: `Borrower ${borrower.address}`,
+//       action: 'borrowed',
+//       amount: amountUSDCToBorrow,
+//       coin: 'USDC',
+//       unit: 'USD',
+//       ...userGlobalDataAfter,
+//     });
+
+//     expect(userGlobalDataAfter.currentLiquidationThreshold.toString()).to.be.bignumber.equal(
+//       '7500',
+//       'Invalid liquidation threshold'
+//     );
+
+//     await advanceBlock((await timeLatest()).plus(3600 * 120).toNumber());
+
+//     //approve protocol to access borrower wallet
+//     await usdc.connect(borrower.signer).approve(pool.address, APPROVAL_AMOUNT_LENDING_POOL);
+
+//     await expect(
+//       pool
+//         .connect(borrower.signer)
+//         .repay(usdc.address, amountUSDCToBorrow, RateMode.Variable, borrower.address)
+//     ).to.not.be.reverted;
+
+//     printUserAccountData({
+//       user: `Borrower ${borrower.address}`,
+//       action: 'borrowed',
+//       amount: amountUSDCToBorrow,
+//       coin: 'USDC',
+//       unit: 'USD',
+//       ...(await pool.getUserAccountData(borrower.address)),
+//     });
+//   });
+// });

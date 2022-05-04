be8c603e513ad9653d75274a917820b186a67dfa
atozICT20 <atozict20@gmail.com>
2022-05-04 15:37:42 -0400

    implement swap logic for yield

--- contracts/protocol/vault/YieldManager.sol
+++ contracts/protocol/vault/YieldManager.sol
@@ -6,9 +6,15 @@ import {Errors} from '../libraries/helpers/Errors.sol';
 import {VersionedInitializable} from '../../protocol/libraries/sturdy-upgradeability/VersionedInitializable.sol';
 import {ILendingPoolAddressesProvider} from '../../interfaces/ILendingPoolAddressesProvider.sol';
 import {ILendingPool} from '../../interfaces/ILendingPool.sol';
+import {IPriceOracleGetter} from '../../interfaces/IPriceOracleGetter.sol';
+import {ISwapRouter} from '../../interfaces/ISwapRouter.sol';
+import {IERC20Detailed} from '../../dependencies/openzeppelin/contracts/IERC20Detailed.sol';
 import {Ownable} from '../../dependencies/openzeppelin/contracts/Ownable.sol';
 import {PercentageMath} from '../libraries/math/PercentageMath.sol';
 import {SafeMath} from '../../dependencies/openzeppelin/contracts/SafeMath.sol';
+import {TransferHelper} from '../libraries/helpers/TransferHelper.sol';
+import {UniswapAdapter} from '../libraries/swap/UniswapAdapter.sol';
+import {CurveswapAdapter} from '../libraries/swap/CurveswapAdapter.sol';
 
 /**
  * @title YieldManager
@@ -33,6 +39,14 @@ contract YieldManager is VersionedInitializable, Ownable {
 
   uint256 public constant VAULT_REVISION = 0x1;
 
+  address public _exchangeToken;
+
+  // tokenIn -> tokenOut -> Curve Pool Address
+  mapping(address => mapping(address => address)) internal _curvePools;
+
+  uint256 public constant UNISWAP_FEE = 500;
+  uint256 public constant SLIPPAGE = 200;
+
   modifier onlyAdmin() {
     require(_addressesProvider.getPoolAdmin() == msg.sender, Errors.CALLER_NOT_POOL_ADMIN);
     _;
@@ -44,6 +58,11 @@ contract YieldManager is VersionedInitializable, Ownable {
    **/
   function initialize(ILendingPoolAddressesProvider _provider) public initializer {
     _addressesProvider = _provider;
+    _exchangeToken = _addressesProvider.getAddress('USDC');
+  }
+
+  function setExchangeToken(address _token) external onlyOwner {
+    _exchangeToken = _token;
   }
 
   function getRevision() internal pure override returns (uint256) {
@@ -56,15 +75,55 @@ contract YieldManager is VersionedInitializable, Ownable {
   }
 
   /**
+   * @dev Function to set Curve Pool address for the swap
+   * @param _tokenIn The address of token being exchanged
+   * @param _tokenOut The address of token being received
+   * @param _pool The address of the Curve pool to use for the swap
+   */
+  function setCurvePool(
+    address _tokenIn,
+    address _tokenOut,
+    address _pool
+  ) external onlyOwner {
+    _curvePools[_tokenIn][_tokenOut] = _pool;
+  }
+
+  /**
+   * @dev Function to get Curve Pool address for the swap
+   * @param _tokenIn The address of token being sent
+   * @param _tokenOut The address of token being received
+   */
+  function getCurvePool(address _tokenIn, address _tokenOut) external view returns (address) {
+    return _curvePools[_tokenIn][_tokenOut];
+  }
+
+  /**
    * @dev Distribute the yield of assets to suppliers.
+   *      1. convert asset to exchange token(for now it's USDC) via Uniswap
+   *      2. convert exchange token to other stables via Curve
+   *      3. deposit to pool for suppliers
    * @param _offset assets array's start offset.
    * @param _count assets array's count when perform distribution.
    **/
   function distributeYield(uint256 _offset, uint256 _count) external onlyAdmin {
-    // 1. convert from all external assets to USDC via uniswap
-    // ex: 3CRV -> USDC, WETH -> USDC
-    // 2. convert from USDC to other stable assets via curve swap
-    // 3. deposit Yield to pool for suppliers
+    // 1. convert from asset to exchange token via uniswap
+    for (uint256 i = 0; i < _count; i++) {
+      address asset = _assetsList[_offset + i];
+      require(asset != address(0), Errors.UL_INVALID_INDEX);
+      uint256 _amount = IERC20Detailed(asset).balanceOf(address(this));
+      _convertAssetToExchangeToken(asset, _amount);
+    }
+    uint256 exchangedAmount = IERC20Detailed(_exchangeToken).balanceOf(address(this));
+
+    // 2. convert from exchange token to other stable assets via curve swap
+    AssetYield[] memory assetYields = _getAssetYields(exchangedAmount);
+    for (uint256 i = 0; i < assetYields.length; i++) {
+      if (assetYields[i].amount > 0) {
+        uint256 _amount = _convertToStableCoin(assetYields[i].asset, assetYields[i].amount);
+        // 3. deposit Yield to pool for suppliers
+        _depositYield(assetYields[i].asset, _amount);
+      }
+    }
   }
 
   /**
@@ -99,5 +158,55 @@ contract YieldManager is VersionedInitializable, Ownable {
     }
 
     return assetYields;
+  }
+
+  /**
+   * @dev Convert asset to exchange token via Uniswap
+   * @param asset The address of asset being exchanged
+   * @param amount The amount of asset being exchanged
+   */
+  function _convertAssetToExchangeToken(address asset, uint256 amount) internal {
+    address WETH = _addressesProvider.getAddress('WETH');
+    bool useEthPath = asset != WETH;
+
+    UniswapAdapter.swapExactTokensForTokens(
+      _addressesProvider,
+      asset,
+      _exchangeToken,
+      amount,
+      UNISWAP_FEE,
+      SLIPPAGE,
+      useEthPath
+    );
+  }
+
+  /**
+   * @dev The function to convert from exchange token to stable coin via Curve
+   * @param _tokenOut The address of stable coin
+   * @param _amount The amount of exchange token being sent
+   * @return receivedAmount The amount of stable coin converted
+   */
+  function _convertToStableCoin(address _tokenOut, uint256 _amount)
+    internal
+    returns (uint256 receivedAmount)
+  {
+    address _pool = _curvePools[_exchangeToken][_tokenOut];
+    CurveswapAdapter.swapExactTokensForTokens(
+      _addressesProvider,
+      _pool,
+      _exchangeToken,
+      _tokenOut,
+      _amount,
+      SLIPPAGE
+    );
+  }
+
+  /**
+   * @dev The function to deposit yield to pool for suppliers
+   * @param _asset The address of yield asset
+   * @param _amount The mount of asset
+   */
+  function _depositYield(address _asset, uint256 _amount) internal {
+    ILendingPool(_addressesProvider.getLendingPool()).depositYield(_asset, _amount);
   }
 }

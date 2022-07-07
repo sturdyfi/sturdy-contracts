0d1f4ddbc21edf19ec3bd64b1ed56df8dddd143b
atozICT20 <atozict20@gmail.com>
2022-07-07 15:15:22 -0400

    implement leverage feature

--- contracts/protocol/leverage/GeneralLevSwap.sol
+++ contracts/protocol/leverage/GeneralLevSwap.sol
@@ -2,37 +2,40 @@
 pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
-import {VersionedInitializable} from '../libraries/sturdy-upgradeability/VersionedInitializable.sol';
 import {IERC20} from '../../dependencies/openzeppelin/contracts/IERC20.sol';
 import {IERC20Detailed} from '../../dependencies/openzeppelin/contracts/IERC20Detailed.sol';
 import {SafeERC20} from '../../dependencies/openzeppelin/contracts/SafeERC20.sol';
 import {IPriceOracleGetter} from '../../interfaces/IPriceOracleGetter.sol';
-import {ICollateralAdapter} from '../../interfaces/ICollateralAdapter.sol';
 import {ILendingPool} from '../../interfaces/ILendingPool.sol';
 import {ILendingPoolAddressesProvider} from '../../interfaces/ILendingPoolAddressesProvider.sol';
-import {ReserveConfiguration} from '../libraries/configuration/ReserveConfiguration.sol';
 import {PercentageMath} from '../libraries/math/PercentageMath.sol';
-import {DataTypes} from '../libraries/types/DataTypes.sol';
 import {GeneralVault} from '../vault/GeneralVault.sol';
 import {Errors} from '../libraries/helpers/Errors.sol';
-import 'hardhat/console.sol';
 
 contract GeneralLevSwap {
   using SafeERC20 for IERC20;
   using PercentageMath for uint256;
-  using ReserveConfiguration for DataTypes.ReserveConfigurationMap;
 
   uint256 public constant USE_VARIABLE_DEBT = 2;
 
   address public immutable COLLATERAL; // The addrss of external asset
+
+  uint256 public immutable DECIMALS; // The collateral decimals
+
   address public immutable VAULT; // The address of vault
+
   ILendingPoolAddressesProvider internal _addressesProvider;
 
-  uint256 public constant SAFE_BUFFER = 1000;
+  uint256 public constant SAFE_BUFFER = 5000;
 
   mapping(address => bool) ENABLED_STABLE_COINS;
 
-  event EnterPosition(uint256 amount, uint256 iterations, address indexed borrowedCoin);
+  event EnterPosition(
+    uint256 amount,
+    uint256 iterations,
+    uint256 ltv,
+    address indexed borrowedCoin
+  );
 
   /**
    * @param asset The external asset ex. wFTM
@@ -50,6 +53,7 @@ contract GeneralLevSwap {
     );
 
     COLLATERAL = asset;
+    DECIMALS = IERC20Detailed(asset).decimals();
     VAULT = vault;
     _addressesProvider = ILendingPoolAddressesProvider(_provider);
     IERC20(COLLATERAL).approve(vault, type(uint256).max);
@@ -63,34 +67,6 @@ contract GeneralLevSwap {
   }
 
   /**
-   * @return LTV of ASSET in 4 decimals ex. 82.5% == 8250
-   */
-  function getLTV() public view returns (uint256 LTV) {
-    ICollateralAdapter collateralAdapter = ICollateralAdapter(
-      _addressesProvider.getAddress('COLLATERAL_ADAPTER')
-    );
-    address internalAsset = collateralAdapter.getInternalCollateralAsset(COLLATERAL);
-
-    DataTypes.ReserveConfigurationMap memory configuration = ILendingPool(
-      _addressesProvider.getLendingPool()
-    ).getConfiguration(internalAsset);
-
-    (LTV, , , , ) = configuration.getParamsMemory();
-  }
-
-  function getAToken() public view returns (address) {
-    ICollateralAdapter collateralAdapter = ICollateralAdapter(
-      _addressesProvider.getAddress('COLLATERAL_ADAPTER')
-    );
-    address internalAsset = collateralAdapter.getInternalCollateralAsset(COLLATERAL);
-
-    DataTypes.ReserveData memory reserve = ILendingPool(_addressesProvider.getLendingPool())
-      .getReserveData(internalAsset);
-
-    return reserve.aTokenAddress;
-  }
-
-  /**
    * @return The asset price in ETH according to Sturdy PriceOracle
    */
   function getAssetPrice(address _asset) public view returns (uint256) {
@@ -101,19 +77,18 @@ contract GeneralLevSwap {
   /**
    * @param principal - The amount of collateral
    * @param iterations - Loop count
-   * @param stableAsset - The stable coin address to borrow when leverage works
+   * @param ltv - The loan to value of the asset in 4 decimals ex. 82.5% == 8250
+   * @param stableAsset - The borrowing stable asset address when leverage works
    */
   function enterPosition(
     uint256 principal,
     uint256 iterations,
+    uint256 ltv,
     address stableAsset
   ) public {
     require(principal > 0, Errors.LS_SWAP_AMOUNT_NOT_GT_0);
     require(ENABLED_STABLE_COINS[stableAsset], Errors.LS_STABLE_COIN_NOT_SUPPORTED);
-    require(
-      IERC20Detailed(COLLATERAL).balanceOf(msg.sender) >= principal,
-      Errors.LS_SUPPLY_NOT_ALLOWED
-    );
+    require(IERC20(COLLATERAL).balanceOf(msg.sender) >= principal, Errors.LS_SUPPLY_NOT_ALLOWED);
 
     IERC20(COLLATERAL).safeTransferFrom(msg.sender, address(this), principal);
 
@@ -121,8 +96,14 @@ contract GeneralLevSwap {
 
     uint256 _suppliedAmount = principal;
     uint256 _borrowAmount = 0;
+    uint256 _stableAssetDecimals = IERC20Detailed(stableAsset).decimals();
     for (uint256 i = 0; i < iterations; i++) {
-      _borrowAmount = _calcBorrowableAmount(_suppliedAmount, stableAsset);
+      _borrowAmount = _calcBorrowableAmount(
+        _suppliedAmount,
+        ltv,
+        stableAsset,
+        _stableAssetDecimals
+      );
       if (_borrowAmount > 0) {
         // borrow stable coin
         _borrow(stableAsset, _borrowAmount);
@@ -133,7 +114,7 @@ contract GeneralLevSwap {
       }
     }
 
-    emit EnterPosition(principal, iterations, stableAsset);
+    emit EnterPosition(principal, iterations, ltv, stableAsset);
   }
 
   function _supply(uint256 amount) internal {
@@ -150,18 +131,20 @@ contract GeneralLevSwap {
     );
   }
 
-  function _calcBorrowableAmount(uint256 collateralAmount, address borrowAsset)
-    internal
-    view
-    returns (uint256)
-  {
-    uint256 ltv = getLTV();
-    uint256 availableBorrowsETH = (collateralAmount *
-      getAssetPrice(COLLATERAL).percentMul(ltv).percentMul(99_00)) /
-      (10**IERC20Detailed(COLLATERAL).decimals());
+  function _calcBorrowableAmount(
+    uint256 collateralAmount,
+    uint256 ltv,
+    address borrowAsset,
+    uint256 assetDecimals
+  ) internal view returns (uint256) {
+    uint256 availableBorrowsETH = (collateralAmount * getAssetPrice(COLLATERAL).percentMul(ltv)) /
+      (10**DECIMALS);
 
-    uint256 availableBorrowsAsset = (availableBorrowsETH *
-      (10**IERC20Detailed(borrowAsset).decimals())) / getAssetPrice(borrowAsset);
+    availableBorrowsETH = availableBorrowsETH > SAFE_BUFFER ? availableBorrowsETH - SAFE_BUFFER : 0;
+
+    uint256 availableBorrowsAsset = (availableBorrowsETH * (10**assetDecimals)) /
+      getAssetPrice(borrowAsset);
+
     return availableBorrowsAsset;
   }
 

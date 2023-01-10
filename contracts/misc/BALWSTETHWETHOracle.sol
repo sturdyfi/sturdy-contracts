1a87199d25f815f0bf2a8345de7dcd1955664be4
iris112 <nglayi99@gmail.com>
2023-01-10 13:34:14 -0600

    [BAL-SUGG] Add the oracle check feature before calc price

--- contracts/misc/BALWSTETHWETHOracle.sol
+++ contracts/misc/BALWSTETHWETHOracle.sol
@@ -2,7 +2,9 @@
 // Using the same Copyleft License as in the original Repository
 pragma solidity ^0.8.0;
 
+import '@balancer-labs/v2-interfaces/contracts/vault/IVault.sol';
 import './interfaces/IOracle.sol';
+import './interfaces/IOracleValidate.sol';
 import '../interfaces/IChainlinkAggregator.sol';
 import '../interfaces/IBalancerStablePool.sol';
 import '../interfaces/IWstETH.sol';
@@ -12,12 +14,13 @@ import {Math} from '../dependencies/openzeppelin/contracts/Math.sol';
 /**
  * @dev Oracle contract for BALWSTETHWETH LP Token
  */
-contract BALWSTETHWETHOracle is IOracle {
+contract BALWSTETHWETHOracle is IOracle, IOracleValidate {
   IBalancerStablePool private constant BALWSTETHWETH =
     IBalancerStablePool(0x32296969Ef14EB0c6d29669C550D4a0449130230);
   IChainlinkAggregator private constant STETH =
     IChainlinkAggregator(0x86392dC19c0b719886221c78AB11eb8Cf5c52812);
   IWstETH private constant WSTETH = IWstETH(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0);
+  address private constant BALANCER_VAULT = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
 
   /**
    * @dev Get LP Token Price
@@ -48,5 +51,19 @@ contract BALWSTETHWETHOracle is IOracle {
   /// @inheritdoc IOracle
   function latestAnswer() external view override returns (int256 rate) {
     return int256(_get());
+  }
+
+  // Check the oracle
+  /// @inheritdoc IOracleValidate
+  function check() external {
+    IVault.UserBalanceOp[] memory ops = new IVault.UserBalanceOp[](1);
+    ops[0] = IVault.UserBalanceOp(
+      IVault.UserBalanceOpKind.WITHDRAW_INTERNAL,
+      IAsset(address(1)),
+      0,
+      address(this),
+      payable(address(this))
+    );
+    IVault(BALANCER_VAULT).manageUserBalance(ops);
   }
 }

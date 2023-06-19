74c05695c81f30e78e804f9d9407b348d0e6c4f0
iris112 <nglayi99@gmail.com>
2023-06-19 06:21:28 -0500

    update balancer reth-weth lp token price calculation

--- contracts/misc/BALRETHWETHOracle.sol
+++ contracts/misc/BALRETHWETHOracle.sol
@@ -7,6 +7,7 @@ import {VaultReentrancyLib} from '@balancer-labs/v2-pool-utils/contracts/lib/Vau
 import './interfaces/IOracle.sol';
 import '../interfaces/IChainlinkAggregator.sol';
 import '../interfaces/IBalancerStablePool.sol';
+import '../interfaces/IRocketTokenRETH.sol';
 import {Errors} from '../protocol/libraries/helpers/Errors.sol';
 import {Math} from '../dependencies/openzeppelin/contracts/Math.sol';
 
@@ -14,8 +15,10 @@ import {Math} from '../dependencies/openzeppelin/contracts/Math.sol';
  * @dev Oracle contract for BALRETHWETH LP Token
  */
 contract BALRETHWETHOracle is IOracle {
-  IChainlinkAggregator private constant RETH =
+  IChainlinkAggregator private constant RETH_PRICE =
     IChainlinkAggregator(0x536218f9E9Eb48863970252233c8F271f554C2d0);
+  IRocketTokenRETH private constant RETH =
+    IRocketTokenRETH(0xae78736Cd615f374D3085123A210448E74Fc6393);
   IBalancerStablePool private constant BALRETHWETH =
     IBalancerStablePool(0x1E19CF2D73a72Ef1332C882F20534B6519Be0276);
   address private constant BALANCER_VAULT = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
@@ -27,13 +30,15 @@ contract BALRETHWETHOracle is IOracle {
     // Check the oracle (re-entrancy)
     VaultReentrancyLib.ensureNotInVaultContext(IVault(BALANCER_VAULT));
 
-    (, int256 rETHPrice, , uint256 updatedAt, ) = RETH.latestRoundData();
+    (, int256 rETHPrice, , uint256 updatedAt, ) = RETH_PRICE.latestRoundData();
 
-    require(RETH.decimals() == 18, Errors.O_WRONG_PRICE);
+    require(RETH_PRICE.decimals() == 18, Errors.O_WRONG_PRICE);
     require(updatedAt > block.timestamp - 1 days, Errors.O_WRONG_PRICE);
     require(rETHPrice > 0, Errors.O_WRONG_PRICE);
 
-    uint256 minValue = Math.min(uint256(rETHPrice), 1e18);
+    uint256 redemptionPrice = RETH.getExchangeRate();
+    uint256 normPrice = (uint256(rETHPrice) * 1e18) / redemptionPrice;
+    uint256 minValue = Math.min(normPrice, 1e18);
 
     return (BALRETHWETH.getRate() * minValue) / 1e18;
   }
